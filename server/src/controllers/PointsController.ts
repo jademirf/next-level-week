83a40e122fbb57ca9bcc3fddd060bf4737543b50
Jademir de Moura <jademir@policiacivil.pe.gov.br>
2020-06-05 18:34:19 -0300

    upload de imagem no server

--- server/src/controllers/PointsController.ts
+++ server/src/controllers/PointsController.ts
@@ -17,7 +17,7 @@ class PointsController {
     const trx = await knex.transaction()
     
     const point = {
-      image: 'https://images.unsplash.com/photo-1529686786214-513a02fe2a64?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
+      image: req.file.filename,
       name,
       email,
       whatsapp,
@@ -30,12 +30,15 @@ class PointsController {
 
     const point_id = insertedIds[0]
 
-    const pointItems = items.map( (item_id: number) => {
-       return {
-           item_id,
-           point_id
-       }
-    })
+    const pointItems = items
+      .split(',')
+      .map((item: string) => Number(item.trim()))
+      .map( (item_id: number) => {
+        return {
+            item_id,
+            point_id
+        }
+      })
 
     await trx('point_items').insert(pointItems)
 
